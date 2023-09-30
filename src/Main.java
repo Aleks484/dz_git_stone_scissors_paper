8ad844817194f75d270513fbf6000a531bb0d93b
STALKER1 <ALEKS484A@GMAIL.RU>
2023-09-30 18:12:48 +0300

    Доработка игры 1

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int compChoice = random.nextInt(choices.length);//генерирует случайный индекс от 0 до размера массива
-        int player;
-        int exit;
-        System.out.println("Начало игры");
-        System.out.println("Выберете вартант");
-        System.out.println("0. камень\n"+"1. ножницы\n"+"2. бумага\n");
-        //int playerChoise = scanner.nextInt()-1;
-        int playerChoise = scanner.nextInt();
-        System.out.println(compChoice);
-        if(playerChoise == compChoice){
-            System.out.println("ничья");
+        compChoice = compChoice+1;
+
+            System.out.println("Начало игры");
+            System.out.println("Выберете вартант");
+            System.out.println("1. камень\n" + "2. ножницы\n" + "3. бумага\n" );
+            int playerChoice = scanner.nextInt();
+            if (playerChoice == 1) {
+                System.out.println(playerChoice + ". (playerChoice) камень");
+
+            }
+            if (playerChoice == 2) {
+                System.out.println(playerChoice + ". (playerChoice) ножницы");
+            }
+            if (playerChoice == 3) {
+                System.out.println(playerChoice + ". (playerChoice) бумага");
+            }
+
+            if (compChoice == 1) {
+                System.out.println(compChoice + ". (compChoice) камень");
+            }
+            if (compChoice == 2) {
+                System.out.println(compChoice + ". (compChoice)  ножницы");
+            }
+            if (compChoice == 3) {
+                System.out.println(compChoice + ". (compChoice)  бумага");
+            }
+
+            if (playerChoice == compChoice) {
+                System.out.println("ничья");
+            } else if (playerChoice == 0 && compChoice == 1 || playerChoice == 1 && compChoice == 2 || playerChoice == 2 && compChoice == 0) {
+                System.out.println("Победил игрок");
+            } else {
+                System.out.println("Победил компьютер");
+
+            }
         }
-        else if (playerChoise == 0 && compChoice == 1 || playerChoise == 1 && compChoice == 2 || playerChoise == 2 && compChoice == 0 ){
-            System.out.println("Победил игрок");
-        } else {
-            System.out.println("Победил компьютер");
-        }
-    }
-}
+   }

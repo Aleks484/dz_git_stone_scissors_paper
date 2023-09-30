98937d9c8f583c6356142aaa7c5271344ef35a0c
STALKER1 <ALEKS484A@GMAIL.RU>
2023-09-30 15:18:21 +0300

    Реализовали игру

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String [] choices = {"камень","ножницы","бумага"};
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int compChoice = random.nextInt(choices.length);//генерирует случайный индекс от 0 до размера массива
+        int player;
+        int exit;
+        System.out.println("Начало игры");
+        System.out.println("Выберете вартант");
+        System.out.println("0. камень\n"+"1. ножницы\n"+"2. бумага\n");
+        //int playerChoise = scanner.nextInt()-1;
+        int playerChoise = scanner.nextInt();
+        System.out.println(compChoice);
+        if(playerChoise == compChoice){
+            System.out.println("ничья");
+        }
+        else if (playerChoise == 0 && compChoice == 1 || playerChoise == 1 && compChoice == 2 || playerChoise == 2 && compChoice == 0 ){
+            System.out.println("Победил игрок");
+        } else {
+            System.out.println("Победил компьютер");
+        }
     }
 }
